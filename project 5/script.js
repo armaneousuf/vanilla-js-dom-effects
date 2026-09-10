dbc7f4d72a1b1efc3eece4fcd7cc173a933af666
armaneousuf <armaneousuf97@gmail.com>
2026-09-10 13:33:04 +0600

    update: 2026-09-10 13:33

--- project 5/script.js
+++ project 5/script.js
@@ -30,18 +30,26 @@ const ARR = [
   },
 
   {
-    dp: "https://images.unsplash.com/photo-1788888882364-7cf34cec52f5?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    dp: "https://images.unsplash.com/photo-1787744688812-ebcafe7a5e85?q=80&w=405&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     story:
       "https://images.unsplash.com/photo-1788855753462-a37191c29e51?q=80&w=388&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   }
 ];
 
-let clutter = "";
+let storiesHTML = "";
+let stories = document.querySelector('.stories');
 
-ARR.forEach(function (elem) {
-  clutter += `<div class="story">
-                <img src="${elem.dp}" alt="">
+ARR.forEach(function (story, index) {
+  storiesHTML += `<div class="story">
+                <img id="${index}" src="${story.dp}" alt="">
             </div>`;
 });
-document.querySelector('.stories').innerHTML = clutter
-console.log(clutter);
+
+stories.innerHTML = storiesHTML;
+stories.addEventListener('click', (details) => {
+    document.querySelector('.full-screen').style.display = 'block';
+    document.querySelector('.full-screen').style.backgroundImage = `url(${ARR[details.target.id].story})`;
+    setTimeout(() => {
+      document.querySelector('.full-screen').style.display = 'none'
+    }, 3000);
+})
