e6dc6397ab7c889cada0a28911d12bb68e5a09dd
armaneousuf <armaneousuf97@gmail.com>
2026-09-12 12:52:24 +0600

    update: 2026-09-12 12:52

---
+++ project 6/practice-1.js
@@ -0,0 +1,37 @@
+let Bank = (function (){
+    let TotalBalance = 120000;
+
+    function checkBalance(){
+        console.log(TotalBalance);
+    }
+
+    function setBalance(value){
+        TotalBalance = value
+    }
+
+    function addBalance(value){
+        TotalBalance += value;
+    }
+
+    function withdraw(value){
+        if(value <= TotalBalance){
+            TotalBalance -= value;
+            console.log(TotalBalance);
+        }
+    }
+
+    return {
+        checkBalance, 
+        setBalance,
+        addBalance,
+        withdraw
+    }
+})();
+
+Bank.checkBalance();
+Bank.withdraw(20000);
+Bank.setBalance(50000);
+Bank.checkBalance();
+Bank.addBalance(25000);
+Bank.checkBalance();
+
